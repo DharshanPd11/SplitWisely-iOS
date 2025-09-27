74d718003a5ef51e745bd5176bdae58609291fa1
priyadharshan.r <101190088+DharshanPd11@users.noreply.github.com>
2025-09-27 20:21:50 +0530

    initial project setup

---
+++ SplitWisely/SplitWisely/SplitWiselyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitWiselyApp.swift
+//  SplitWisely
+//
+//  Created by Priyadharshan Raja on 27/09/25.
+//
+
+import SwiftUI
+
+@main
+struct SplitWiselyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
